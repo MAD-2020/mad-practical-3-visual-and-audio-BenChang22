34b5bc095f2ada66e499e6bed41db79425d258f2
Zong Hao Ben Chang <s10185167@connect.np.edu.sg>
2020-05-11 19:54:14 +0800

    completed

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -2,7 +2,10 @@ package sg.edu.np.mad.week3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        MediaPlayer videophone;
+        videophone = MediaPlayer.create(this, R.raw.baby_laughing);
+        videophone.start();
+
+        VideoView rabbit= findViewById(R.id.rabbitVideo);
+        rabbit.setVideoURI(
+                Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.rabbid)
+        );
+        rabbit.start();
     }
 }
